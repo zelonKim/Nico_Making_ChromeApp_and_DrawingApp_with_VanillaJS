a91951594786b3bd9d2a473f1c483d1ab12b7e00
zelonKim <ksz18601@gmail.com>
2023-05-05 09:50:38 +0900

    Recap

---
+++ WELCOME TO JAVASCRIPT/app.js
@@ -0,0 +1,8 @@
+const toBuy = ["potato", "tomato","pizza"]
+console.log(toBuy)  // ['potato', 'tomato', 'pizza']
+
+toBuy[2] = "water"
+console.log(toBuy) // ['potato', 'tomato', 'water']
+
+toBuy.push("meat")
+console.log(toBuy) // ['potato', 'tomato', 'water', 'meat']
