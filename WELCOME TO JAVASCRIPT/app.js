4520ba62753798e8833b41c3e9d59fbfdae67b50
zelonKim <ksz18601@gmail.com>
2023-05-05 13:16:25 +0900

    Conditionals

--- WELCOME TO JAVASCRIPT/app.js
+++ WELCOME TO JAVASCRIPT/app.js
@@ -1,104 +1,22 @@
 /*
-const calculator = {
-    add: function(a, b) {
-        console.log(a + b);
-    },
-    minus: function(a, b) {
-        console.log(a - b);
-    },
-    divide: function(a, b) {
-        console.log(a / b)
-    },
-    multi: function(a, b) {
-        console.log(a * b)
-    }
-};
-console.log(calculator.add(10,20)) // 출력: 30  undefined
+const age = prompt("how old are you?"); // 입력: 25
+
+console.log(age)  // 출력: 25
+console.log(typeof age)  // 출력: string
+
+ageNum = parseInt(age)
+console.log(ageNum)  // 출력: 25
+console.log(typeof ageNum)  // 출력: number
 */
 
 /*
-const myAge = 56;
-function calculateKoreanAge(ageOfForeigner) {
-    ageOfForeigner + 2;
-}
-const koreanAge = calculateKoreanAge(myAge)
-console.log(koreanAge) // 출력: undefined
+const age = prompt("how old are you?"); // 입력: 김성진
+
+ageNum = parseInt(age)
+console.log(ageNum) // 출력: NaN (Not a Number)
 */
 
-/*
-const myAge = 56;
-function calculateKoreanAge(ageOfForeigner) {
-    return ageOfForeigner + 2;
-}
-const koreanAge = calculateKoreanAge(myAge)
-console.log(koreanAge) // 출력: 58
-*/
 
-/*
-const myAge = 56;
-function calculateKoreanAge(ageOfForeigner) {
-    ageOfForeigner + 2;
-    return "hello"
-}
-const koreanAge = calculateKoreanAge(myAge)
-console.log(koreanAge) // 출력: hello
-*/
+const age = prompt("how old are you?");  // 입력: 김성진
 
-/*
-const calculator = {
-    add: function(a, b) {
-        return a + b;
-    },
-    minus: function(a, b) {
-        return a - b;
-    },
-    divide: function(a, b) {
-        return a / b
-    },
-    multi: function(a, b) {
-        return a * b
-    }
-};
-const plusResult = calculator.add(2,3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.multi(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-
-console.log(plusResult)  // 출력: 5
-console.log(minusResult) // 출력: -5
-console.log(timesResult) // 출력: -50
-console.log(divideResult) // 출력: -10
-*/
-
-/*
-const calcuator = {
-    add: function(a, b) {
-        console.log(a + b);
-    }
-};
-
-calcuator.add(10,20)  // 출력: 30
-console.log(calcuator.add(10,20))
-*/
-
-/*
-const calculator = {
-    add: function(a, b) {
-        console.log("hello")
-        return a + b;
-        console.log("bye bye")
-    }
-};
-const result = calculator.add(10, 20)
-console.log(result)  // 출력: hello  30
-*/
-
-const calculator = {
-    add: function(a, b) {
-        console.log("bye bye")
-        return a + b;
-        console.log("hello")
-    }
-};
-const result = calculator.add(10, 20)
-console.log(result)  // 출력: bye bye 30
+console.log(age) // 출력: 김성진
