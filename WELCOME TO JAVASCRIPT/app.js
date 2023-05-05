a424040ede4113889471bde67b74ce50e80847bf
zelonKim <ksz18601@gmail.com>
2023-05-05 11:12:40 +0900

    Recap2

--- WELCOME TO JAVASCRIPT/app.js
+++ WELCOME TO JAVASCRIPT/app.js
@@ -1,8 +1,43 @@
-const toBuy = ["potato", "tomato","pizza"]
-console.log(toBuy)  // ['potato', 'tomato', 'pizza']
+const player = {
+    name: 'Nico',
+    age: 30
+};
+console.log(player)  // 출력: {name: 'Nico', age: 30}
 
-toBuy[2] = "water"
-console.log(toBuy) // ['potato', 'tomato', 'water']
+player.name = 'seongjin';
+console.log(player)  // 출력: {name: 'seongjin', age: 30}
 
-toBuy.push("meat")
-console.log(toBuy) // ['potato', 'tomato', 'water', 'meat']
+player.weight = 60;
+console.log(player) // 출력: {name: 'seongjin', age: 30, weight: 60}
+
+
+
+function minusFive(num) {
+    console.log(num-5);
+}
+
+minusFive(5,15,25,35,45)  // 출력: 0
+minusFive(15,25,35,45,5)  // 출력: 10
+minusFive(25,35,45,5,15)  // 출력: 20
+
+
+
+const calculator = {
+    add: function(a, b) {
+        console.log(a + b);
+    },
+    minus: function(a, b) {
+        console.log(a - b);
+    },
+    divide: function(a, b) {
+        console.log(a / b)
+    },
+    multi: function(a, b) {
+        console.log(a * b)
+    }
+};
+
+calculator.add(10,20)  // 출력: 30
+calculator.minus(10,20)  // 출력: -10
+calculator.divide(10,20) // 출력: 0.5
+calculator.multi(10,20) // 출력: 200
