811ba6d846f26d3be0d955bb58d1a3a6c6ddf17f
zelonKim <ksz18601@gmail.com>
2023-05-06 13:51:04 +0900

    HTML in Javascript2

--- JAVASCRIPT ON THE BROWSER/app.js
+++ JAVASCRIPT ON THE BROWSER/app.js
@@ -1,1 +1,7 @@
-document.title = "nice to meet you"
+const elem = document.getElementById("hello") // HTML에서 id="hello"인 요소를 찾아서 가져옴.
+console.log(elem) // 출력: <h1 id="hello">Grab me</h1>
+
+elem.innerText = "how are you" // <h1 id="hello">how are you</h1>로 바꿔줌.
+
+console.log(elem.id)  // "hello"
+console.log(elem.className) // "niceToMeetYa"
