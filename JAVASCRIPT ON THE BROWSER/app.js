691f3ec712adc366c8d0252264b747d825dbab57
zelonKim <ksz18601@gmail.com>
2023-05-10 09:04:20 +0900

    Searching For Elements

--- JAVASCRIPT ON THE BROWSER/app.js
+++ JAVASCRIPT ON THE BROWSER/app.js
@@ -1,3 +1,4 @@
+/*
 const elem = document.getElementById("hello") // HTML에서 id="hello"인 요소를 찾아서 가져옴.
 console.log(elem) // 출력: <h1 id="hello">Grab me</h1>
 
@@ -5,3 +6,41 @@ elem.innerText = "how are you" // <h1 id="hello">how are you</h1>로 바꿔줌.
 
 console.log(elem.id)  // "hello"
 console.log(elem.className) // "niceToMeetYa"
+*/
+
+/*
+console.log(document.getElementsByClassName("Hi")) // HTMLCollection(4) [h2.hi, h2.hi, h3.hi, h3.hi]
+console.log(document.getElementsByClassName("Hi")[0]) // <h2 class="hi">hi steve</h2>
+
+
+console.log(document.getElementsByTagName("h2")) // HTMLCollection(2) [h2.hi, h2.hi]
+console.log(document.getElementsByTagName("h2")[0]) // <h2 class="hi">hi steve</h2>
+
+console.log(document.getElementsByTagName("h3")) // HTMLCollection(2) [h3.hi, h3.hi]
+console.log(document.getElementsByTagName("h3")[0]) // <h3 class="hi">hi ariana</h3>
+*/
+
+
+/* 
+title = document.querySelector(".hello h2");
+
+console.log(title) // <h2 class="Hi">hi steve</h2>
+*/
+
+
+/* 
+title = document.querySelectorAll(".hello h2");
+
+console.log(title)  // NodeList(2) [h2.Hi, h2.Hi]
+*/
+
+
+/*
+ title = document.querySelectorAll(".hello h2")[1];
+
+console.log(title) // <h2 class="Hi">hi john</h2>
+*/
+
+
+
+
