c49b764b4d00993fe3267811e2a42f8e15554703
zelonKim <ksz18601@gmail.com>
2023-05-10 13:03:36 +0900

    CSS in Javascript

--- JAVASCRIPT ON THE BROWSER/app.js
+++ JAVASCRIPT ON THE BROWSER/app.js
@@ -1,82 +1,77 @@
 /* 
-title = document.querySelector("#hello")
-
-title.style.color = 'green' // 요소의 글자색을 초록색으로 바꿈.
-
-console.dir(title) 
-*/
-
-
-/* 
-title = document.querySelector("#hello")
+h1 = document.querySelector(".hello h1")
 
 function handleTitleClick() {
-    console.log("title was clicked!")
+    if (h1.style.color === "blue") {
+        h1.style.color = "tomato";
+    } else {
+        h1.style.color = "blue"
+    }
 }
 
-title.addEventListener("click", handleTitleClick)  
+h1.addEventListener("click", handleTitleClick);
 */
-
-
 /* 
-title = document.querySelector("#hello")
+h1 = document.querySelector(".hello h1")
 
 function handleTitleClick() {
-    title.style.color = "green"
+    h1.className = "active"; // h1요소의 class를 "active"로 바꿈.
 }
+// CSS 파일
+//   .active {
+//    color: tomato;
+//     }
 
-title.addEventListener("click", handleTitleClick) 
+h1.addEventListener("click", handleTitleClick)
 */
+
+//----------------------
+
 
 /* 
-title = document.querySelector("#hello")
+const h1 = document.querySelector(".hello h1")
+console.log(h1) // <h1 class="niceToMeetU">Grab me</h1>
 
-function handleMouseEnter() {
-    console.log("mouse is here!");
+function handleTitleClick() {
+    const clickedClass = "clicked";
+    if(h1.className === clickedClass) {
+        h1.className = "niceToMeetU";
+        console.log(h1) // <h1 class="niceToMeetU">Grab me</h1>
+    } else {
+        h1.className = clickedClass;  // 원래 있던 niceToMeetU 클래스는 제거됨.
+        console.log(h1)  // <h1 class="clicked">Grab me</h1>
+    }
 }
-
-title.addEventListener("mouseenter", handleMouseEnter); 
+h1.addEventListener("click", handleTitleClick) 
 */
+
 
 /*
- title = document.querySelector("#hello")
+const h1 = document.querySelector(".hello h1")
+console.log(h1) // <h1 class="niceToMeetU">Grab me</h1>
 
 function handleTitleClick() {
-    console.log("title was clicked!")
+    const clickedClass = "clicked";
+    if(h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
+        console.log(h1) // <h1 class="niceToMeetU">Grab me</h1>
+    } else {
+        h1.classList.add(clickedClass);  // 원래 있던 niceToMeetU 클래스가 유지됨.
+        console.log(h1) // <h1 class="niceToMeetU clicked">Grab me</h1>
+    }
 }
 
-title.onclick = handleTitleClick
- */
-
-
-/* 
-title = document.querySelector("#hello")
-
-function handleMouseEnter() {
-    console.log("mouse is here!");
-}
-
-title.onmouseenter = handleMouseEnter;
+h1.addEventListener("click", handleTitleClick) 
 */
 
-/* 
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
 
-window.addEventListener("resize", handleWindowResize);
-*/
+//요소.classList.toggle("클래스명")은 위 코드와 똑같이 동작함.
+const h1 = document.querySelector(".hello h1")
+console.log(h1) // <h1 class="niceToMeetU">Grab me</h1>
 
-/* 
-function handleWindowCopy() {
-    alert("copier!");
-}
+function handleTitleClick() {
+    h1.classList.toggle("clicked");
+        console.log(h1) // <h1 class="niceToMeetU clicked">Grab me</h1>
+    }
 
-window.addEventListener("copy", handleWindowCopy)
-*/
-
-function handleWindowOffline() {
-    alert("no Wifi");
-}
-
-window.addEventListener("offline", handleWindowOffline)
+h1.addEventListener("click", handleTitleClick) 
